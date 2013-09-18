e704736c29d37baf5d3dc949c60fa6c5e8e6f96a
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2013-09-18 19:34:04 +0100

    entprop consructor

--- lib/Drupal/action_link/Plugin/StateCycler/EntityProperty.php
+++ lib/Drupal/action_link/Plugin/StateCycler/EntityProperty.php
@@ -10,10 +10,28 @@ namespace Drupal\action_link\Plugin\StateCycler;
 use Drupal\action_link\StateCyclerInterface;
 
 /**
- * entity property toggle, eg published / unpublished
+ * Toggles an entity property, eg published / unpublished
  */
 class EntityProperty implements StateCyclerInterface {
-  
+
+  /**
+   * Constructor.
+   *
+   * @param $target_entity
+   *  The target entity we act on.
+   * @param $parameters
+   *  An array of parameters. The format is specific to this plugin:
+   *  - 'property': The name of the property that is to be toggled.
+   */
+  function __construct($target_entity, $parameters) {
+    // TODO! this should be passed in by the config entity.
+    // For now, cheat!
+    $parameters['property'] = 'status';
+
+    $this->target_entity = $target_entity;
+    $this->toggle_property = $parameters['property'];
+  }
+
   function actionIsValid() {
     // Does the action actually make sense? 
     // eg. 
