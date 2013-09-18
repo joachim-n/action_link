89440a6508d6768dc2321de99816f4dfd4582dce
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2013-09-18 13:11:43 +0100

    added buildLink to config interface

--- lib/Drupal/action_link/ActionLinkConfigInterface.php
+++ lib/Drupal/action_link/ActionLinkConfigInterface.php
@@ -13,6 +13,22 @@ namespace Drupal\action_link;
  */
 interface ActionLinkConfigInterface {
 
+  // Get my style plugin
 
+  // Get my state cycler
+
+  /**
+   * Generate the render array for the action link.
+   *
+   * This will confer with the StateCycler plugin to figure out which state
+   * the link should allow cycling to.
+   *
+   * @param $entity
+   *  The entity to create a link for.
+   *
+   * @return
+   *  A render array containing the link.
+   */
+  public function buildLink($entity);
 
 }
