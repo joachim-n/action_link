c43f3a076e9813eee83c289321188f318ea5db04
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2022-11-21 17:37:40 +0000

    iterface.

--- src/Entity/ActionLinkInterface.php
+++ src/Entity/ActionLinkInterface.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\action_link\Entity;
 
+use Drupal\action_link\Plugin\StateAction\StateActionInterface;
 use Drupal\Core\Config\Entity\ConfigEntityInterface;
 use Drupal\Core\Entity\EntityWithPluginCollectionInterface;
 
@@ -9,5 +10,8 @@ use Drupal\Core\Entity\EntityWithPluginCollectionInterface;
  * Interface for Action Link entities.
  */
 interface ActionLinkInterface extends ConfigEntityInterface, EntityWithPluginCollectionInterface {
+
+  public function getStateActionPlugin(): StateActionInterface;
+
 
 }
