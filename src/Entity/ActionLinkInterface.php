246744c821e130dacb4447c563b12999530c4cdd
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2022-11-23 16:40:37 +0000

    add to interface.

--- src/Entity/ActionLinkInterface.php
+++ src/Entity/ActionLinkInterface.php
@@ -5,6 +5,8 @@ namespace Drupal\action_link\Entity;
 use Drupal\action_link\Plugin\StateAction\StateActionInterface;
 use Drupal\Core\Config\Entity\ConfigEntityInterface;
 use Drupal\Core\Entity\EntityWithPluginCollectionInterface;
+use Drupal\Core\Link;
+use Drupal\Core\Session\AccountInterface;
 
 /**
  * Interface for Action Link entities.
@@ -13,5 +15,18 @@ interface ActionLinkInterface extends ConfigEntityInterface, EntityWithPluginCol
 
   public function getStateActionPlugin(): StateActionInterface;
 
+  /**
+   * Gets a link for the action link.
+   *
+   * @param \Drupal\Core\Session\AccountInterface $user
+   *   The user to get the link for.
+   * @param mixed ...$parameters
+   *   The parameters for the link. These are specific to the state action
+   *   plugin.
+   *
+   * @return \Drupal\Core\Link
+   *   A link object.
+   */
+  public function getLink(AccountInterface $user, ...$parameters): Link;
 
 }
