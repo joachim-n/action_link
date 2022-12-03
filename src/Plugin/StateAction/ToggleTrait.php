6126ccc8f1ac03fda5c651fc21d74da6adc18f14
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2022-12-03 13:10:50 +0000

    toggle messages.

--- src/Plugin/StateAction/ToggleTrait.php
+++ src/Plugin/StateAction/ToggleTrait.php
@@ -12,6 +12,12 @@ use Drupal\Core\StringTranslation\TranslatableMarkup;
 trait ToggleTrait {
 
   public function buildTextsConfigurationForm($labels_form, FormStateInterface $form_state) {
+    $labels_form['state']['true'] = [
+      '#type' => 'details',
+      '#open' => TRUE,
+      '#title' => t('Texts for setting the toggle'),
+    ];
+
     $labels_form['state']['true']['link_label'] = [
       '#type' => 'textfield',
       '#title' => t('Link label for setting the toggle'),
@@ -19,10 +25,26 @@ trait ToggleTrait {
       // todo basic defaults.
     ];
 
+    $labels_form['state']['true']['message'] = [
+      '#type' => 'textfield',
+      '#title' => t('Message when setting the toggle'),
+    ];
+
+    $labels_form['state']['false'] = [
+      '#type' => 'details',
+      '#open' => TRUE,
+      '#title' => t('Texts for unsetting the toggle'),
+    ];
+
     $labels_form['state']['false']['link_label'] = [
       '#type' => 'textfield',
       '#title' => t('Link label for removing the toggle'),
       '#required' => TRUE,
+    ];
+
+    $labels_form['state']['false']['message'] = [
+      '#type' => 'textfield',
+      '#title' => t('Message when unsetting the toggle'),
     ];
 
     return $labels_form;
