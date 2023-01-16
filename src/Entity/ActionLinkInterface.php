4bc1031a0ebc6e365cfebde9f36f3178a7da9880
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2023-01-16 09:48:54 +0000

    docs, interface.

--- src/Entity/ActionLinkInterface.php
+++ src/Entity/ActionLinkInterface.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\action_link\Entity;
 
+use Drupal\action_link\Plugin\ActionLinkStyle\ActionLinkStyleInterface;
 use Drupal\action_link\Plugin\StateAction\StateActionInterface;
 use Drupal\Core\Config\Entity\ConfigEntityInterface;
 use Drupal\Core\Entity\EntityWithPluginCollectionInterface;
@@ -13,7 +14,21 @@ use Drupal\Core\Session\AccountInterface;
  */
 interface ActionLinkInterface extends ConfigEntityInterface, EntityWithPluginCollectionInterface {
 
+  /**
+   * Gets the state action plugin for this entity.
+   *
+   * @return \Drupal\action_link\Plugin\StateAction\StateActionInterface
+   *   The configured plugin.
+   */
   public function getStateActionPlugin(): StateActionInterface;
+
+  /**
+   * Gets the link style plugin for this entity.
+   *
+   * @return \Drupal\action_link\Plugin\ActionLinkStyle\ActionLinkStyleInterface
+   *   The plugin.
+   */
+  public function getLinkStylePlugin(): ActionLinkStyleInterface;
 
   /**
    * Gets a link for the action link.
