df7dcc8776556451ea747b4fe1eb7df38d2d3335
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2023-01-16 07:05:00 +0000

    docs

--- src/Plugin/ActionLinkStyle/ActionLinkStyleInterface.php
+++ src/Plugin/ActionLinkStyle/ActionLinkStyleInterface.php
@@ -7,6 +7,17 @@ use Drupal\Component\Plugin\PluginInspectionInterface;
 
 /**
  * Interface for Action Link Style plugins.
+ *
+ * An action link style plugin determines how the links for an action link
+ * entity will behave in the UI.
+ *
+ * For example, a link could be a plain HTML link which reloads the current
+ * page, or a JavaScript link which receives an AJAX response.
+ *
+ * An action link's link style setting affects how links are output, but does
+ * not restrict which link styles are responded to. This means that any link
+ * style URL will work for an action link entity. This is to allow for graceful
+ * degradation of JavaScript line.
  */
 interface ActionLinkStyleInterface extends PluginInspectionInterface, DerivativeInspectionInterface {
 
