81434c651daef28d34b82534f6bba054479f7f97
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2023-02-24 07:24:43 +0000

    clean up

--- src/StateActionManager.php
+++ src/StateActionManager.php
@@ -37,6 +37,4 @@ class StateActionManager extends DefaultPluginManager {
     $this->setCacheBackend($cache_backend, 'state_action_plugins');
   }
 
-  // TODO: check that if 'directions' is defined IFF there's also a 'direction' dynamic param defined.
-
 }
