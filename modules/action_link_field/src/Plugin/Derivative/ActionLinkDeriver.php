d0b0e476c229e03043e61383922679420904713c
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2023-03-03 16:33:19 +0000

    argh fix circular!

--- modules/action_link_field/src/Plugin/Derivative/ActionLinkDeriver.php
+++ modules/action_link_field/src/Plugin/Derivative/ActionLinkDeriver.php
@@ -13,6 +13,9 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 /**
  * Deriver for action_link computed field plugins.
  *
+ * Derived computed fields are defined as either base or bundle bundles, to
+ * match the field affected by the action link.
+ *
  * @see TODO.
  */
 class ActionLinkDeriver extends DeriverBase implements ContainerDeriverInterface {
@@ -67,11 +70,57 @@ class ActionLinkDeriver extends DeriverBase implements ContainerDeriverInterface
       // }
 
       if (empty($action_link_state_action_plugin->getConfiguration()['entity_type_id'])) {
-        throw new PluginException("Missing entity type on $action_link_entity_id");
+        throw new PluginException("Missing entity_type_id on $action_link_entity_id");
       }
+      if (empty($action_link_state_action_plugin->getConfiguration()['field'])) {
+        throw new PluginException("Missing field on $action_link_entity_id");
+      }
+
+      // The entity type whose field value the action link changes.
+      $host_entity_type_id = $action_link_state_action_plugin->getConfiguration()['entity_type_id'];
+      // The name of the field that the action link changes.
+      $field_name = $action_link_state_action_plugin->getConfiguration()['field'];
+
       // dump($action_link_state_action_plugin->getConfiguration()['entity_type_id']);
 
       // dump($action_link_state_action_plugin);
+
+      // We need to determine whether the field that the action link acts on is
+      // either a base field or a bundle field, and if a bundle field, which
+      // bundles it is on. However, to get this information from the entity
+      // field manager to cause circularity, as this deriver is called when
+      // getting plugin definitions to define base and bundle fields. So we
+      // cheat, and use the keyvalue store that the entity field manager uses
+      // to get the bundle fields for the field map. This is a hack, as it's
+      // using something that's internal to that service and could be changed or
+      // removed without notice!!!
+      // @see \Drupal\Core\Entity\EntityFieldManager::getFieldMap().
+      $this->keyValueFactory = \Drupal::service('keyvalue');
+      $bundle_field_maps = $this->keyValueFactory->get('entity.definitions.bundle_field_map')->getAll();
+      // dump($bundle_field_maps);
+
+      // dump($field_name);
+
+      // $field_map = \Drupal::service('entity_field.manager')->getFieldMap();
+
+      // return;
+
+      // Derived computed fields are defined as either base or bundle bundles,
+      // to match the action link's target field.
+      // FUCK -- CIRCULAR. FUCK FUCK FUCK.
+      // $base_field_definitions = \Drupal::service('entity_field.manager')->getBaseFieldDefinitions($host_entity_type_id);
+      if (isset($bundle_field_maps[$host_entity_type_id][$field_name])) {
+        $scope = 'bundle';
+
+        // $field_map = \Drupal::service('entity_field.manager')->getFieldMap();
+
+        $bundles = $bundle_field_maps[$host_entity_type_id][$field_name]['bundles'];
+        // dump($bundles);
+      }
+      else {
+        $scope = 'base';
+        $bundles = [];
+      }
 
       $this->derivatives[$action_link_entity_id] = [
         // NOT,
@@ -81,10 +130,9 @@ class ActionLinkDeriver extends DeriverBase implements ContainerDeriverInterface
         'label' => $action_link_entity->label() . ' ' . t('action link'),
         'attach' => [
           'field_name' => "action_link_{$action_link_entity_id}",
-          'scope' => 'base', // TODO, match the action link's field.
+          'scope' => $scope,
           'entity_types' => [
-            // TODO bundles if bundle scope.
-            $action_link_state_action_plugin->getConfiguration()['entity_type_id'] => [],
+            $host_entity_type_id => $bundles,
           ],
         ],
       ] + $base_plugin_definition;
