bfc3ab6455e4eae419fe085f6da9c1751e183c16
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2023-03-04 17:56:27 +0000

    clean

--- modules/action_link_field/src/Plugin/Derivative/ActionLinkDeriver.php
+++ modules/action_link_field/src/Plugin/Derivative/ActionLinkDeriver.php
@@ -74,44 +74,8 @@ class ActionLinkDeriver extends DeriverBase implements ContainerDeriverInterface
       // The name of the field that the action link changes.
       $field_name = $action_link_state_action_plugin->getConfiguration()['field'];
 
-      // dump($action_link_state_action_plugin->getConfiguration()['entity_type_id']);
-
-      // dump($action_link_state_action_plugin);
-
-      // We need to determine whether the field that the action link acts on is
-      // either a base field or a bundle field, and if a bundle field, which
-      // bundles it is on. However, to get this information from the entity
-      // field manager to cause circularity, as this deriver is called when
-      // getting plugin definitions to define base and bundle fields. So we
-      // cheat, and use the keyvalue store that the entity field manager uses to
-      // get the bundle fields for the field map. Problems with this
-      // approach:
-      // - This is a hack, as it's using something that's internal to
-      //   that service and could be changed or removed without notice!!!
-      // - It won't work if the action link's target field is neither a base
-      //   field nor a config field, such as a bundle field defined in code.
-      //   That's because in this case, it won't be in the field map, and will
-      //   be incorrectly deduced to be a base field.
-      // @see \Drupal\Core\Entity\EntityFieldManager::getFieldMap().
-      // $this->keyValueFactory = \Drupal::service('keyvalue');
-      // $bundle_field_maps = $this->keyValueFactory->get('entity.definitions.bundle_field_map')->getAll();
-
-      // if (isset($bundle_field_maps[$host_entity_type_id][$field_name])) {
-      //   $scope = 'bundle';
-
-      //   $bundles = $bundle_field_maps[$host_entity_type_id][$field_name]['bundles'];
-      // }
-      // else {
-      //   $scope = 'base';
-      //   $bundles = [];
-      // }
-
       $this->derivatives[$action_link_entity_id] = [
-        // NOT,
-        // $action_link_state_action_plugin->getPluginDefinition()['label']
-        // NO, remove suffix???
-        // REALLY need admin label for manage display page!!
-        'label' => $action_link_entity->label() . ' ' . t('action link'),
+        'label' => $action_link_entity->label(),
         'attach' => [
           // Omit scope and bundles, which may or may not apply, as we can't
           // determine these here without circularity.
