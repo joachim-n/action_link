9010518e928e65605897fff3f59f3532af77cfa6
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2023-05-03 21:18:00 +0100

    clean up

--- tests/modules/action_link_browser_test/src/Controller/ActionLinkBrowserTestController.php
+++ tests/modules/action_link_browser_test/src/Controller/ActionLinkBrowserTestController.php
@@ -22,8 +22,6 @@ class ActionLinkBrowserTestController {
 
     $build = [];
 
-    $entity_type_manager = \Drupal::service('entity_type.manager');
-
     $dynamic_parameter_names = $action_link->getStateActionPlugin()->getDynamicParameterNames();
     $parameters = [];
     if ($dynamic_parameter_names) {
@@ -37,49 +35,6 @@ class ActionLinkBrowserTestController {
       '#type' => 'container',
     ];
     $build['linkset']['links'] = $action_link->buildLinkSet(NULL, ...$parameters);
-
-    // TEMP!
-    return $build;
-
-
-    // Individual links.
-    foreach ($action_link->getStateActionPlugin()->getDirections() as $direction => $label) {
-      $build['singles'][$direction]  = [
-        '#type' => 'container',
-      ];
-      $build['singles'][$direction]['link'] = $action_link->buildSingleLink($direction, $user, ...$parameters);
-    }
-
-    return $build;
-
-    // TODO: Links on other node.
-    $node = $entity_type_manager->getStorage('node')->load(4);
-    $build['other'] = $action_link->buildLinkSet($user, $node);
-
-    return $build;
-
-    // $action_links = $entity_type_manager->getStorage('action_link')->loadMultiple();
-    // $action_links = [$action_links['test_date']];
-
-    foreach ($action_links as $action_link_id => $action_link) {
-      // dsm($action_link);
-      $build[$action_link_id . '4'] = [
-        '#type' => 'container',
-      ];
-
-      $build[$action_link_id . '4']['links'] = $action_link->buildLinkSet($user, $node);
-
-      // break;
-    }
-
-
-    // Test repeat links!
-    // $build['repeat'] = [
-    //   '#type' => 'container',
-    // ];
-
-    // $build['repeat']['links'] = $action_link->buildLinkSet($user, $node);
-
 
     return $build;
   }
