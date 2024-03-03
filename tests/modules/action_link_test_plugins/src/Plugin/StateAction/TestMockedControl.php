46889ebac7499cdf32cc437337d1312bc3e062a2
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2024-03-03 20:46:12 +0000

    Added docs to TestMockedControl test plugin.

--- tests/modules/action_link_test_plugins/src/Plugin/StateAction/TestMockedControl.php
+++ tests/modules/action_link_test_plugins/src/Plugin/StateAction/TestMockedControl.php
@@ -10,6 +10,16 @@ use Drupal\Core\Session\AccountInterface;
 /**
  * Test plugin which returns access and operability based on state value.
  *
+ * Uses the following state values as return values from methods:
+ *  - test_mocked_control:getNextStateName
+ *  - test_mocked_control:checkOperability
+ *  - test_mocked_control:checkPermissionStateAccess
+ *  - test_mocked_control:checkOperandGeneralAccess
+ *  - test_mocked_control:checkOperandStateAccess
+ *
+ * When advanceState() is called, the test_mocked_control:set_state state key
+ * is set with the new state.
+ *
  * @StateAction(
  *   id = "test_mocked_control",
  *   label = @Translation("Test mocked control"),
