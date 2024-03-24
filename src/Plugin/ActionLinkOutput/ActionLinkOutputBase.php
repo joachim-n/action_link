14bbdf811bf6292359e7938d52207e7593505d78
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2024-03-24 07:27:18 +0000

    generated code + tweaks

---
+++ src/Plugin/ActionLinkOutput/ActionLinkOutputBase.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Drupal\action_link\Plugin\ActionLinkOutput;
+
+use Drupal\Component\Plugin\PluginBase;
+
+/**
+ * Base class for Action Link Output plugins.
+ */
+abstract class ActionLinkOutputBase extends PluginBase implements ActionLinkOutputInterface {
+
+}
