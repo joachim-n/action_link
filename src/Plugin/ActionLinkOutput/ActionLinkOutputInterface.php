4ebc29c094983051fd4518e7c395e3aa84a554cc
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2024-03-28 14:18:44 +0000

    interafce.

--- src/Plugin/ActionLinkOutput/ActionLinkOutputInterface.php
+++ src/Plugin/ActionLinkOutput/ActionLinkOutputInterface.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\action_link\Plugin\ActionLinkOutput;
 
+use Drupal\action_link\Entity\ActionLinkInterface;
 use Drupal\Component\Plugin\DerivativeInspectionInterface;
 use Drupal\Component\Plugin\PluginInspectionInterface;
 
@@ -9,5 +10,17 @@ use Drupal\Component\Plugin\PluginInspectionInterface;
  * Interface for Action Link Output plugins.
  */
 interface ActionLinkOutputInterface extends PluginInspectionInterface, DerivativeInspectionInterface {
+
+  /**
+   * Determines whether the plugin is usable with the action link entity.
+   *
+   * @param \Drupal\action_link\Entity\ActionLinkInterface $action_link
+   *   An action link entity.
+   *
+   * @return bool
+   *   TRUE if the output plugin can be used with the given action link entity,
+   *   FALSE otherwise.
+   */
+  public static function applies(ActionLinkInterface $action_link): bool;
 
 }
