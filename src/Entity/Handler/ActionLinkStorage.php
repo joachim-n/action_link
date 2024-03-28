ac9dc794192033dba4c204bbb565c0addaa1d169
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2024-03-28 14:13:25 +0000

    added storage method

--- src/Entity/Handler/ActionLinkStorage.php
+++ src/Entity/Handler/ActionLinkStorage.php
@@ -9,4 +9,20 @@ use Drupal\Core\Config\Entity\ConfigEntityStorage;
  */
 class ActionLinkStorage extends ConfigEntityStorage {
 
+  /**
+   * Loads action link entities that are configured to use the output plugin.
+   *
+   * @param string $output_plugin_id
+   *   An output plugin ID.
+   *
+   * @return array
+   *   An array of all action link entities that are configured to output using
+   *   the given output plugin.
+   */
+  public function loadByUsingOutput(string $output_plugin_id): array {
+    // @todo Optimise this, ideally using lookup_keys.
+    $action_links = $this->loadMultiple();
+    return array_filter($action_links, fn ($action_link) => isset($action_link->get('output')[$output_plugin_id]));
+  }
+
 }
