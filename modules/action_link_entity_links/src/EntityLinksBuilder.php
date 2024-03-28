dc13c07830ee43f9052c432ac274739d8169bfc3
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2024-03-28 22:48:09 +0000

    BIG rework of LB, one LB placeholder **PER** direction

--- modules/action_link_entity_links/src/EntityLinksBuilder.php
+++ modules/action_link_entity_links/src/EntityLinksBuilder.php
@@ -40,18 +40,52 @@ class EntityLinksBuilder {
    * @param array $context
    */
   public function entityLinksAlter(array &$links, ContentEntityInterface $entity, array &$context) {
+    // @todo Make view modes configurable in the plugin.
     $view_mode = $context['view_mode'];
 
-    $placeholder = Crypt::hashBase64('action_link-entity_links-' . $entity->getEntityTypeId() . '-' . $entity->id() . '-' . $view_mode);
+    $action_link_entities = $this->entityTypeManager->getStorage('action_link')->loadByUsingOutput('entity_links');
+
+    $action_link_links = [];
+
+    $links['#attached'] ??= [];
+
+    /** @var \Drupal\action_link\Entity\ActionLinkInterface $action_link_entity */
+    foreach ($action_link_entities as $action_link_id => $action_link_entity) {
+      foreach ($action_link_entity->getStateActionPlugin()->getDirections() as $direction => $label) {
+        $placeholder = Crypt::hashBase64(implode('-', [
+          'action_link-entity_links',
+          $entity->getEntityTypeId(),
+          $entity->id(),
+          $action_link_id,
+          $direction,
+        ]));
+
+        $action_link_links["action_link:$action_link_id:$direction"] = [
+          'title' => $placeholder,
+        ];
+
+        $links['#attached']['placeholders'][$placeholder] = [
+          '#lazy_builder' => [
+            // The lazy builder callback and parameters.
+            'action_link_entity_links.builder:entityLinksLazyBuilder',
+            [
+              $entity->getEntityTypeId(),
+              $entity->id(),
+              $action_link_id,
+              $direction,
+            ],
+          ],
+        ];
+      }
+    }
+
+    // $placeholder = Crypt::hashBase64('action_link-entity_links-' . $entity->getEntityTypeId() . '-' . $entity->id() . '-' . $view_mode);
 
     // TODO! CACHE STUFF!
+    // TODO chec $action_link_links not empty!
     $links['action_link'] = [
       '#theme' => 'links__node__action_link',
-      '#links' => [
-        'action_link' => [
-          'title' => $placeholder,
-        ],
-      ],
+      '#links' => $action_link_links,
     ];
 
     $links['dummy'] = [
@@ -64,65 +98,57 @@ class EntityLinksBuilder {
       '#attributes' => ['class' => ['links', 'inline']],
     ];
 
-    $links['#attached'] ??= [];
-    $links['#attached']['placeholders'][$placeholder] = [
-      '#lazy_builder' => [
-        // The lazy builder callback and parameters.
-        'action_link_entity_links.builder:entityLinksLazyBuilder',
-        [
-          $entity->getEntityTypeId(),
-          $entity->id(),
-          $view_mode,
-        ],
-      ],
-    ];
+    dsm($links);
   }
 
   /**
    * Undocumented function
    */
   #[TrustedCallback]
-  public function entityLinksLazyBuilder($entity_type_id, $entity_id, $view_mode) {
-    // @todo Make view modes configurable in the plugin.
+  public function entityLinksLazyBuilder($entity_type_id, $entity_id, $action_link_id, $direction) {
     $entity = $this->entityTypeManager->getStorage($entity_type_id)->load($entity_id);
+    $action_link_entity = $this->entityTypeManager->getStorage('action_link')->load($action_link_id);
     $user = \Drupal::currentUser();
 
-    $action_link_entities = $this->entityTypeManager->getStorage('action_link')->loadByUsingOutput('entity_links');
-    foreach ($action_link_entities as $action_link_entity) {
-      // Replace the ajax plugin with our altered version.
-      // @todo Add a getter for this?
-      if ($action_link_entity->get('link_style') == 'ajax') {
-        $action_link_entity->set('link_style',  'ajax_entity_links');
-      }
-
-      $state_action_plugin = $action_link_entity->getStateActionPlugin();
-
-      // This is a total hack: we get the render array for the action link, then
-      // cannibalise it for bits to put in the render array for the entity links.
-      // @todo Refactor the action link rendering methods on StateActionBase to
-      // give us things that are more suitable here.
-      $action_links = $state_action_plugin->buildLinkArray(
-        $action_link_entity,
-        $user,
-        [
-          'entity' => $entity->id(),
-        ],
-      );
-
-      $action_link_links = [];
-      foreach (Element::children($action_links) as $direction) {
-        $action_link_links['action_link' . ':' . $action_link_entity->id() . ':' . $direction] = [
-          'title' => $action_links[$direction]['#link']['#title'],
-          'url' => $action_links[$direction]['#link']['#url'],
-          'attributes' => $action_links[$direction]['#link']['#attributes'],
-        ];
-
-        // We can't set attributes on the LI element at this point, so stash them in
-        // a fake key in the link attributes for
-        // action_link_entity_links_preprocess_links() to retrieve.
-        $action_link_links['action_link' . ':' . $action_link_entity->id() . ':' . $direction]['attributes']['li_class'] = $action_links[$direction]['#attributes']['class'];
-      }
+    // Replace the ajax plugin with our altered version.
+    // @todo Add a getter for this?
+    if ($action_link_entity->get('link_style') == 'ajax') {
+      $action_link_entity->set('link_style',  'ajax_entity_links');
     }
+
+    $state_action_plugin = $action_link_entity->getStateActionPlugin();
+
+    // This is a total hack: we get the render array for the action link, then
+    // cannibalise it for bits to put in the render array for the entity links.
+    // @todo Refactor the action link rendering methods on StateActionBase to
+    // give us things that are more suitable here.
+    $action_link_build = $state_action_plugin->buildSingleLink(
+      $action_link_entity,
+      $direction,
+      $user,
+      [
+        'entity' => $entity->id(),
+      ],
+    );
+
+    // dsm($action_link_build);
+    // Why this array??
+    return $action_link_build[$direction];
+
+    //   $action_link_links = [];
+    //   foreach (Element::children($action_links) as $direction) {
+    //     $action_link_links['action_link' . ':' . $action_link_entity->id() . ':' . $direction] = [
+    //       'title' => $action_links[$direction]['#link']['#title'],
+    //       'url' => $action_links[$direction]['#link']['#url'],
+    //       'attributes' => $action_links[$direction]['#link']['#attributes'],
+    //     ];
+
+    //     // We can't set attributes on the LI element at this point, so stash them in
+    //     // a fake key in the link attributes for
+    //     // action_link_entity_links_preprocess_links() to retrieve.
+    //     $action_link_links['action_link' . ':' . $action_link_entity->id() . ':' . $direction]['attributes']['li_class'] = $action_links[$direction]['#attributes']['class'];
+    //   }
+    // }
 
     // dsm(func_get_args());
     return [
