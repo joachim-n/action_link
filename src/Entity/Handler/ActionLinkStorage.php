38c01160820cc0a33aa739c31cf4e30316288fac
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2024-03-28 11:36:40 +0000

    add storage

---
+++ src/Entity/Handler/ActionLinkStorage.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Drupal\action_link\Entity\Handler;
+
+use Drupal\Core\Config\Entity\ConfigEntityStorage;
+
+/**
+ * Provides the storage handler for the Action Link entity.
+ */
+class ActionLinkStorage extends ConfigEntityStorage {
+
+}
