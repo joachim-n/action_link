55c47d980d1596eb3e536f860fdcc55eda2b12bc
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2025-01-05 14:43:14 +0000

    Fixed missing hook definition in api.php file.

--- action_link.api.php
+++ action_link.api.php
@@ -33,5 +33,16 @@ function hook_action_link_style_info_alter(array &$info) {
 }
 
 /**
+ * Perform alterations on Action Link Output definitions.
+ *
+ * @param array &$info
+ *   Array of information on Action Link Output plugins.
+ */
+function hook_action_link_output_info_alter(array &$info) {
+  // Change the class of the 'foo' plugin.
+  $info['foo']['class'] = SomeOtherClass::class;
+}
+
+/**
  * @} End of "addtogroup hooks".
  */
