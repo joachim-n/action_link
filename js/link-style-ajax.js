0ff8faf8de85863e6d212399b58c911ac3dceb5b
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2025-01-06 22:59:33 +0000

    Removed unused code; waiting styling is handled by core JS.

--- js/link-style-ajax.js
+++ js/link-style-ajax.js
@@ -6,13 +6,8 @@
 (function ($, Drupal, drupalSettings) {
   'use strict';
 
-  // As flag action links are added to the DOM attach a click handler to each
-  // link so that styles can be applied while the XHR is in flight.
   Drupal.behaviors.actionLinkAttach = {
     attach: function (context, settings) {
-      const links = [...context.querySelectorAll('.action-link a')];
-      links.forEach(link => link.addEventListener('click', event => event.target.parentNode.classList.add('action-link-waiting')));
-
       // When a message DOM element is being attached, add an event to remove it
       // after its CSS animation has completed. The behaviours are called for
       // the message element itself.
